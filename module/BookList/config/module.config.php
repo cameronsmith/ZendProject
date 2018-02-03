165ff1ce2f0d8e67abafd1a4e9f56c37d68bc984
Cameron Smith <gitcontrol@outlook.com>
2018-02-03 17:52:52 +0000

    Add basic controllers, views and routing

--- module/BookList/config/module.config.php
+++ module/BookList/config/module.config.php
@@ -1,3 +1,32 @@
 <?php
 
-return [];
+return [
+    'controllers' => [
+        'invokables' => [
+            'BookList\Controller\Book' => 'BookList\Controller\BookController',
+        ]
+    ],
+    'router' => [
+        'routes' => [
+            'book' => [
+                'type' => 'segment',
+                'options' => [
+                    'route' => '/book[/][:action][/:id]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => 'BookList\Controller\Book',
+                        'action' => 'index',
+                    ]
+                ]
+            ]
+        ]
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'book' => __DIR__ . '/../view',
+        ]
+    ]
+];
