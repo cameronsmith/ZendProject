2cb31e525168c10a60e6cb44d62c1627c2b61788
Cameron Smith <gitcontrol@outlook.com>
2018-02-05 17:44:07 +0000

    Add form and some controller logic

--- module/BookList/src/BookList/Controller/BookController.php
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -1,24 +1,81 @@
 <?php namespace BookList\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use BookList\Form\BookForm;
 
 class BookController extends AbstractActionController {
 
+    /**
+     * Show all books.
+     *
+     * @return ViewModel
+     */
     public function indexAction()
     {
-
+        return new ViewModel([
+            'books' => []
+        ]);
     }
 
+    /**
+     * Add a book.
+     *
+     * @return array
+     */
     public function addAction() {
+        $form = new BookForm;
+        $form->get('submit')->setValue('Add');
 
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+
+        }
+
+        return ['form' => $form];
     }
 
+    /**
+     * Edit a book.
+     *
+     * @return array
+     */
     public function editAction() {
+        $form = new BookForm;
+        //$form->bind($book);
+        $form->get('submit')->setValue('Edit');
 
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+
+        }
+
+        return [
+            'id' => $id,
+            'form' => $form
+        ];
     }
 
+    /**
+     * Delete a book.
+     *
+     * @return array|\Zend\Http\Response
+     */
     public function deleteAction() {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('book');
+        }
 
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+
+        }
+
+        return [
+            'id' => $id,
+            'book' => 0,
+        ];
     }
 }
 
